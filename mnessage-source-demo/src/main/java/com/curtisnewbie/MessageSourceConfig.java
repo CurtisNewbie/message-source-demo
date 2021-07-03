175076595556dab83eb2767acc92df35770a5c08
CurtisNewbie <zhuangyongj@gmail.com>
2021-07-04 01:13:25 +0800

    Implement multilingual message source

---
+++ mnessage-source-demo/src/main/java/com/curtisnewbie/MessageSourceConfig.java
@@ -0,0 +1,23 @@
+//package com.curtisnewbie;
+//
+//import org.springframework.context.MessageSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+//
+///**
+// * @author yongjie.zhuang
+// */
+//@Configuration
+//public class MessageSourceConfig {
+//
+//    @Bean
+//    public MessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource
+//                = new ReloadableResourceBundleMessageSource();
+//
+//        messageSource.setBasename("classpath:messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        return messageSource;
+//    }
+//}
---
+++ mnessage-source-demo/src/main/java/com/curtisnewbie/MessageSourceDemoApp.java
@@ -0,0 +1,27 @@
+package com.curtisnewbie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class MessageSourceDemoApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageSourceDemoApp.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(MessageSourceDemoApp.class, args);
+        MessageSource messageSource = context.getBean(MessageSource.class);
+        String msgForCh = messageSource.getMessage("my-message", null, Locale.SIMPLIFIED_CHINESE);
+        logger.info("my-message with locale ch: {}", msgForCh);
+        String msgForUk = messageSource.getMessage("my-message", null, Locale.UK);
+        logger.info("my-message with locale uk: {}", msgForUk);
+
+    }
+
+}
